ab00af979ecc531da9cfeab0f7446fee2bd832f9
Sungpyo <pwsusc10@naver.com>
2022-11-16 02:37:09 +0900

    add router, folder init

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './app.css';
+import NavigationBar from './components/navigationBar';
+import Homepage from './pages/homepage';
+import Login from './pages/login';
 
 function App() {
-  return <h1>hello!</h1>;
+  return (
+
+    <>
+      <NavigationBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Login />}></Route>
+          <Route path='/homepage' exact element={<Homepage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/navigationBar.jsx
@@ -0,0 +1,11 @@
+import style from '../style/navigationBar.module.css';
+
+const NavigationBar = (props) => {
+  return (
+      <div className={style.section}>
+        dsa
+      </div>
+  );
+}
+
+export default NavigationBar;

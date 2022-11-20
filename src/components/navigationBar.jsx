5a4f315bbb7b2bbe8ad3217eabde0533ebef73ab
Sungpyo <pwsusc10@naver.com>
2022-11-21 00:04:22 +0900

    add navbar css

--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -3,7 +3,14 @@ import style from '../style/navigationBar.module.css';
 const NavigationBar = (props) => {
   return (
       <div className={style.section}>
-        dsa
+        <span className={style.logo}>
+          <span className={style.logo_front}>Sport </span>
+          <span className={style.logo_back}>now</span>
+        </span>
+        <span className={style.navbar}>홈</span>
+        <span className={style.navbar}>일정</span>
+        <span className={style.navbar}>검색</span>
+        <span className={style.navbar}>내 정보</span>
       </div>
   );
 }
